421178b73466c6391f9975c39dcd9ae60a7db1f5
Dariel Subiros <dsubiros@gmail.com>
2024-01-30 00:39:02 -0500

    Exercises with Hash Tables
    - Implemented FindFirstNonRepeatedCharacter.
    - Implemented FindFirstRepeatedCharacter.
    - Next is Hash Functions.

--- src/com/codewithmosh/HashTableSection.java
+++ src/com/codewithmosh/HashTableSection.java
@@ -1,7 +1,10 @@
 package com.codewithmosh;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import com.codewithmosh.classes.CharFinder;
 
@@ -9,17 +12,40 @@ public class HashTableSection {
     public static void main(String[] args) {
         System.out.println("Hash Tables Section...\n");
 
-        // intro();
+        // introToHashMaps();
 
         // Exercise: Find the first non repeated word from...
         // a green apple
 
-        var finder = new CharFinder();
-        System.out.println(finder.getFirstNonRepeatingChar("a green apple"));
+        // getFirstNonRepeatingChar();
+
+        // introToSets();
+
+        findFirstRepeatedChar();
 
     }
 
-    private static void intro() {
+    private static void findFirstRepeatedChar() {
+        CharFinder finder = new CharFinder();
+        System.out.println("First repeated char: " + finder.findFirstRepeatedChar("green apple"));
+    }
+
+    private static void introToSets() {
+        Set<Integer> set = new HashSet<>();
+        int[] numbers = { 1, 2, 2, 3, 4, 5, 5 };
+        for (int number : numbers)
+            set.add(number);
+
+        System.out.println("Array: " + Arrays.toString(numbers));
+        System.out.println("Set: " + set);
+    }
+
+    private static void getFirstNonRepeatingChar() {
+        var finder = new CharFinder();
+        System.out.println(finder.findFirstNonRepeatingChar("a green apple"));
+    }
+
+    private static void introToHashMaps() {
         Map<Integer, String> map = new HashMap<>();
         map.put(1, "Mosh");
         map.put(2, "John");
--- src/com/codewithmosh/classes/CharFinder.java
+++ src/com/codewithmosh/classes/CharFinder.java
@@ -1,10 +1,12 @@
 package com.codewithmosh.classes;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class CharFinder {
-    public char getFirstNonRepeatingChar(String str) {
+    public char findFirstNonRepeatingChar(String str) {
         Map<Character, Integer> map = new HashMap<>();
         var chars = str.toCharArray();
 
@@ -20,6 +22,19 @@ public class CharFinder {
                 System.out.println("Solution: \"" + ch + "\"");
                 return ch;
             }
+
+        return Character.MIN_VALUE;
+    }
+
+    public char findFirstRepeatedChar(String str) {
+        Set<Character> set = new HashSet<>();
+
+        for (char ch : str.toCharArray()) {
+            if (set.contains(ch))
+                return ch;
+            else
+                set.add(ch);
+        }
 
         return Character.MIN_VALUE;
     }
